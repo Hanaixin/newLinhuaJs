cf55818d29c0ac87571cfdf35c3ee95b49c30f8e
hanaixin <13673321759>
2019-12-26 15:34:00 +0800

    左侧单向绑定了选项卡页面

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
 /**
  * Created by Administrator on 2019/12/23.
  */
-
---
+++ public/js/page_url.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2019/12/26.
+ */
+var basePath = '/newLinhuaJs/public/pages/';
+var department_list = basePath + 'Department/department_list';
