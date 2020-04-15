bfadb803da309de273bca934c9afe1ca4d3e23b0
hanaixin <13673321759@163.com>
2020-04-15 11:36:45 +0800

    ***

--- public/js/page_url.js
+++ public/js/page_url.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2019/12/26.
  */
-var basePath = '/newLinhuaJs-master/public/pages/';
+var basePath = '/newLinhuaJs/public/pages/';
 var department_list = basePath + 'Department/department_list';
 var add_department = basePath + 'Department/addDepartment';
 var component = basePath + 'component/component';
